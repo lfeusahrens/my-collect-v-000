e8749289f9128ae29aadc8e8db2f7327a784bc4a
Lauren Feusahrens <lrfeusahrens@gmail.com>
2018-10-17 16:15:13 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,4 @@
-array = ["Lauren Feusahrens", "Devon Feusahrens", "Carl Feusahrens", "Lisa Feusahrens"]
-my_collect(array) do |name|
+collection = ["Lauren Feusahrens", "Devon Feusahrens", "Carl Feusahrens", "Lisa Feusahrens"]
+my_collect(collection) do |name|
   name.split(" ").first
 end
