0208db7b4f31bfb09c45eff5e8db908b962ead65
Lauren Feusahrens <lrfeusahrens@gmail.com>
2018-10-17 16:22:30 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(array)
   i = 0
-  result = []
+  collection = []
    while i < array.size
     block_given? 
-    result << yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
-result
+collection 
 end
